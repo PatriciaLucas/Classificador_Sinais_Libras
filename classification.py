17da8084f077c49eacbfbf8289087d06a11d53ab
Tamires Rezende <inc.tamires@gmail.com>
2021-03-02 19:04:42 +0000

    Update classification.py

--- classification.py
+++ classification.py
@@ -58,7 +58,7 @@ def generate_train_test(matriz_path,numsinalizador):
   datatrain = []
   labeltrain = []
 
-  lista_pontos = [2,5,12,15]
+  lista_pontos = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19]
 
   matrizPaths = os.listdir(matriz_path) #nome dos arquivos
   for matriz in matrizPaths: #Exemplo: '10-15Maca_3.npy'
