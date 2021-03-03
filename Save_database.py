298f3f52cd8a2b7abbbc8f8088bd10db46570764
PATRÍCIA DE OLIVEIRA E LUCAS <patelucas@gmail.com>
2021-03-03 17:35:44 +0000

    Update Save_database.py

--- Save_database.py
+++ Save_database.py
@@ -56,19 +56,32 @@ def sliding_window(list_dataset, sinalizador, window):
   return X_train, y_train, X_test, y_test
 
 
-def experiment(list_dataset, list_names_dataset, list_sinalizadores,database_path,sliding):
+def experiment(list_dataset, list_names_dataset, database_path, num_execute, num_sinalizadores=12, leave_one_out=True, form='sinalizador'):
   execute("CREATE TABLE IF NOT EXISTS results(name_model TEXT, dataset TEXT, accuracy FLOAT, precision FLOAT, recall FLOAT, f1 FLOAT)",database_path)
-  if sliding:
-    list_window = np.arange(0,len(list_dataset)).tolist()
-  else:
-    list_window = [0]
-  for sinalizador in list_sinalizadores:
-    for window in list_window:
-      X_train, y_train, X_test, y_test = sliding_window(list_dataset, sinalizador, window)
-      accuracy, precision, recall, f1 = individual(X_train, y_train, X_test, y_test)
-      execute_insert("INSERT INTO results VALUES(?, ?, ?, ?, ?, ?)",('individual', list_names_dataset[window], accuracy, precision, recall, f1),database_path)
+  for exec in range(num_execute):
+      if form == 'sinalizador':
+        list_sinalizadores = []
+        l = np.arange(1,num_sinalizadores+1).tolist()
+        for x in l: list_sinalizadores.append(str(x)) 
+        if leave_one_out:
+          list_window = np.arange(0,len(list_dataset)).tolist()
+        else:
+          list_window = [0]
+        for sinalizador in list_sinalizadores:
+          for window in list_window:
+            X_train, y_train, X_test, y_test = sliding_window(list_dataset, sinalizador, window)
+            accuracy, precision, recall, f1 = individual(X_train, y_train, X_test, y_test)
+            execute_insert("INSERT INTO results VALUES(?, ?, ?, ?, ?, ?)",('individual', list_names_dataset[window], accuracy, precision, recall, f1),database_path)
+      else:
+        X_train, y_train, X_test, y_test = classification.generate_train_test(list_dataset[0], form=None)
+        for dataset in list_dataset[1:]:
+          X_train2, y_train2, X_test2, y_test2 = classification.generate_train_test(dataset, form=None)
+          X_train, y_train, X_test, y_test = concatenate_samples(X_train, y_train, X_test, y_test, X_train2, y_train2, X_test2, y_test2)
+        X_train, y_train = shuffle(X_train, y_train)
+        X_test, y_test = shuffle(X_test, y_test)
+        accuracy, precision, recall, f1 = individual(X_train, y_train, X_test, y_test)
+        execute_insert("INSERT INTO results VALUES(?, ?, ?, ?, ?, ?)",('individual', list_names_dataset[0], accuracy, precision, recall, f1),database_path)
   return
-
 
 def individual(X_train, y_train, X_test, y_test):
   from tcn import compiled_tcn
