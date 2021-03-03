47116fa21781a4156c430949ca53f7f7ccca14a7
PATRÍCIA DE OLIVEIRA E LUCAS <patelucas@gmail.com>
2021-03-03 11:34:10 +0000

    Update Save_database.py

--- Save_database.py
+++ Save_database.py
@@ -43,7 +43,7 @@ def sliding_window(list_dataset, sinalizador, window):
   X_train1, y_train1, X_test1, y_test1 = classification.generate_train_test(list_dataset[0], sinalizador)
   if window == 0: 
     for dataset in list_dataset:
-      X_train2, y_train2, X_test2, y_test2 = classification.generate_train_test(dataset, sinalizador)
+      X_train2, y_train2, X_test2, y_test2 = classification.generate_train_test(dataset, numsinalizador=sinalizador)
       X_train, y_train, X_test, y_test = concatenate_samples(X_train1, y_train1, X_test1, y_test1, X_train2, y_train2, X_test2, y_test2)
   else:
     for dataset in range(len(list_dataset)):
