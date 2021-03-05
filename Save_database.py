a5a837bdcf9bcab691e3a8e489e37c129158357a
PATRÍCIA DE OLIVEIRA E LUCAS <patelucas@gmail.com>
2021-03-05 19:20:37 +0000

    Update Save_database.py

--- Save_database.py
+++ Save_database.py
@@ -88,12 +88,10 @@ def experiment(list_dataset, list_names_dataset, database_path, num_execute, num
 def individual(X_train, y_train, X_test, y_test):
   from tcn import compiled_tcn
   from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-  from keras.callbacks import EarlyStopping
-  call = [EarlyStopping(monitor='loss', mode='min', patience=5, verbose=1),]
   model = compiled_tcn(return_sequences=False,num_feat=150,num_classes=20,nb_filters=32,kernel_size=5,dilations=[2 ** i for i in range(2)],
                        padding='causal',dropout_rate=0,use_batch_norm=True,nb_stacks=1,max_len=X_train[0:1].shape[1],opt='adam',
                        use_skip_connections=True)
-  history = model.fit(X_train, y_train, epochs=100, workers=4, use_multiprocessing=True, verbose=0, callbacks=call)
+  history = model.fit(X_train, y_train, epochs=100, workers=4, use_multiprocessing=True, verbose=0)
   yhat = model.predict(X_test)
   accuracy = model.evaluate(X_test, y_test)[1]
   precision = precision_score(y_test, yhat.squeeze().argmax(axis=1), average='macro')
